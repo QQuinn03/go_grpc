e17969e59295b1fc86f6cc96202d5a905950f669
Qiong Qin <bbqq@Qiongs-MBP-2.verizon.net>
2023-01-09 12:24:01 -0500

    set up framework

---
+++ cmd/server/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "log"
+
+func Run() error {
+	//reponsible for initializing and starting grpc server
+	return nil
+}
+func main() {
+	if err := Run(); err != nil {
+		log.Fatal(err)
+	}
+}
